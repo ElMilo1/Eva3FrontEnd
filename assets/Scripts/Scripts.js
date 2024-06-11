f7fe277d2fd19e0628ecabe1cb38dca34c1d873d
ElMilo1 <70584603+ElMilo1@users.noreply.github.com>
2024-06-11 11:02:02 -0400

    Recuperacion de datos
    se recuperan datos del html al js y se comprueba mediante console.log()

--- assets/Scripts/Scripts.js
+++ assets/Scripts/Scripts.js
@@ -1,3 +1,41 @@
 window.addEventListener("load",()=>{
-    alert("a")
-})
+    document.getElementById("BtnEnviar").addEventListener("click",userData)
+});
+
+const userData =() =>{
+    let eName = document.getElementById("unombre");
+    let vName = eName.value;
+
+    let eSurname = document.getElementById("uapellido");
+    let vSurname = eSurname.value;
+
+    let eRut = document.getElementById("urut");
+    let vRut = eRut.value;
+
+    let vPhone = document.getElementById("telefono").value;
+
+    let vBday = document.getElementById("bday").value;
+
+    let vAge = document.getElementById("uedad").value;
+
+    let vGender = document.querySelector('input[name="sexo"]').value;
+
+    let vExer = document.querySelector('input[name="rutina"]').value;
+
+    let eComment = document.getElementById("comentario");
+    let vComment = eComment.value;
+
+    let user = {
+        Nombre:vName,
+        Apellido:vSurname,
+        Rut:vRut,
+        Telefono:vPhone,
+        Fecha_Nacimiento:vBday,
+        Edad:vAge,
+        Genero:vGender,
+        Ejercicio_Fav:vExer,
+        Comentario:vComment
+    };
+
+    console.log(user);
+}
